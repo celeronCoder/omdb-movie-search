b3c6c46ce12e4d933c755c6b3e3dc5c26fa83539
celeronCoder <khushalbhardwaj0111@gmail.com>
2022-03-30 00:14:36 +0530

    :sparkles: feat: setup mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import {
+	ColorScheme,
+	ColorSchemeProvider,
+	MantineProvider,
+} from "@mantine/core";
+import { useColorScheme, useHotkeys, useLocalStorage } from "@mantine/hooks";
+
+function Main() {
+	return <div>Hell</div>;
+}
+
 function App() {
-	return <div className="App">OMDB</div>;
+	const preferredColorScheme = useColorScheme();
+	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+		key: "mantine-colorscheme",
+		defaultValue: preferredColorScheme,
+	});
+
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+	useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+	return (
+		<ColorSchemeProvider
+			colorScheme={colorScheme}
+			toggleColorScheme={toggleColorScheme}
+		>
+			<MantineProvider theme={{ colorScheme }}>
+				<Main />
+			</MantineProvider>
+		</ColorSchemeProvider>
+	);
 }
 
 export default App;
